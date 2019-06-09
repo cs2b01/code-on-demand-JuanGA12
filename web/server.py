03432b5f50aa950e0e57e637e3004dc70143ef88
Juan Galvez <juan.galvez@utec.edu.pe>
2019-06-08 22:07:15 -0500

    prueba

--- web/server.py
+++ web/server.py
@@ -83,4 +83,4 @@ def authenticate():
 
 if __name__ == '__main__':
     app.secret_key = ".."
-    app.run(port=8080, threaded=True, host=('127.0.0.1'))
+    app.run(port=080, threaded=True, host=('127.0.0.1'))
